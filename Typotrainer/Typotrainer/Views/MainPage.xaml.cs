ab0ccee9201e1c3b8da902a0b8708f82da22592a
s1218918 <s1218918@student.windesheim.nl>
2025-11-19 18:07:38 +0100

    Database service toegevoegd, mainpage laat nu zien als het verbonden is.

--- Typotrainer/Typotrainer/Views/MainPage.xaml.cs
+++ Typotrainer/Typotrainer/Views/MainPage.xaml.cs
@@ -1,38 +1,53 @@
 namespace Typotrainer.Views;
+using Typotrainer.Services;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
+    readonly DatabaseService _db;   // add a field for your service
 
+    public MainPage()
+    {
+        InitializeComponent();
+        _db = new DatabaseService(); // or pass in your connection string here
     }
-		void PageInloggenClicked(object sender, EventArgs e)
-		{
-			SubPage.Content = new PageInloggen();
-		}
-        void PageAanmeldenClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageAanmelden();
-        }
-		void PageDashboardClicked(object sender, EventArgs e)
-		{
-			SubPage.Content = new PageDashboard();
-		}
-        void PageOefeningClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageOefening();
-        }
-		void PageAdaptieveOefeningClicked(object sender, EventArgs e)
-		{
-			SubPage.Content = new PageAdaptieveOefening();
-		}
-        void PageResultatenClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageResultaten();
-        }
-		void PageInstellingenClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageInstellingen();
-        }
+
+    // existing navigation handlers
+    void PageInloggenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageInloggen();
+    }
+    void PageAanmeldenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageAanmelden();
+    }
+    void PageDashboardClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageDashboard();
+    }
+    void PageOefeningClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageOefening();
+    }
+    void PageAdaptieveOefeningClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageAdaptieveOefening();
+    }
+    void PageResultatenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageResultaten();
+    }
+    void PageInstellingenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageInstellingen();
+    }
+
+    // NEW: run DB test automatically when page appears
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        DBLabel.Text = "Testing...";
+        string result = await _db.DatabaseTest();
+        DBLabel.Text = result;
+    }
 }
