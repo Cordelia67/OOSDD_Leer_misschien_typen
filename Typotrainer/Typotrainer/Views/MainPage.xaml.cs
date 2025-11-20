279a5d22b9fcac371451812ec897b1937819cac4
justrealyme <rickertreimert@gmail.com>
2025-11-20 03:08:46 +0100

    Update MainPage.xaml.cs
    functies public gemaakt voor gebruik buiten de mainpage class

--- Typotrainer/Typotrainer/Views/MainPage.xaml.cs
+++ Typotrainer/Typotrainer/Views/MainPage.xaml.cs
@@ -2,37 +2,43 @@ namespace Typotrainer.Views;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-
+    public MainPage()
+    {
+        InitializeComponent();
     }
-		void PageInloggenClicked(object sender, EventArgs e)
-		{
-			SubPage.Content = new PageInloggen();
-		}
-        void PageAanmeldenClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageAanmelden();
-        }
-		void PageDashboardClicked(object sender, EventArgs e)
-		{
-			SubPage.Content = new PageDashboard();
-		}
-        void PageOefeningClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageOefening();
-        }
-		void PageAdaptieveOefeningClicked(object sender, EventArgs e)
-		{
-			SubPage.Content = new PageAdaptieveOefening();
-		}
-        void PageResultatenClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageResultaten();
-        }
-		void PageInstellingenClicked(object sender, EventArgs e)
-        {
-            SubPage.Content = new PageInstellingen();
-        }
+
+    public void PageInloggenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageInloggen();
+    }
+
+    public void PageAanmeldenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageAanmelden();
+    }
+
+    public void PageDashboardClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageDashboard();
+    }
+
+    public void PageOefeningClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageOefening();
+    }
+
+    public void PageAdaptieveOefeningClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageAdaptieveOefening();
+    }
+
+    public void PageResultatenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageResultaten();
+    }
+
+    public void PageInstellingenClicked(object sender, EventArgs e)
+    {
+        SubPage.Content = new PageInstellingen();
+    }
 }
