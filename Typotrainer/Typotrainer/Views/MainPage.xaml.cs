33359b595b72d809cba99e7cf83bf1ba5bccfebf
s1218918 <s1218918@student.windesheim.nl>
2025-12-03 15:43:53 +0100

    Typotrainer label ziet er beter uit, header heeft geen romnde hoeken meer en heeft de juiste padding Style xkey toegevoegd waar nodig

--- Typotrainer/Typotrainer/Views/MainPage.xaml.cs
+++ Typotrainer/Typotrainer/Views/MainPage.xaml.cs
@@ -8,56 +8,67 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         SubPage.Content = new PageDashboard(); // Dashboard als startpagina
+        HeaderTitel.Text = "Dashboard";
     }
 
     public void PageInloggenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageInloggen();
+        HeaderTitel.Text = "Inloggen";
     }
 
     public void PageAanmeldenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageAanmelden();
+        HeaderTitel.Text = "Aanmelden";
     }
 
     public void PageDashboardClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageDashboard();
+        HeaderTitel.Text = "Dashboard";
     }
 
     public void PageOefeningClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageOefening();
+        HeaderTitel.Text = "Oefening";
     }
 
     public void PageAdaptieveOefeningClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageAdaptieveOefening();
+        HeaderTitel.Text = "Adaptieve oefening";
     }
 
     public void PageResultatenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageResultaten();
+        HeaderTitel.Text = "Resultaten";
     }
 
     public void PageInstellingenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageInstellingen();
+        HeaderTitel.Text = "Instellingen";
     }
 
     // Publieke methodes voor navigatie vanuit andere pagina's
     public void ShowDashboard()
     {
         SubPage.Content = new PageDashboard();
+        HeaderTitel.Text = "Dashboard";
     }
 
     public void ShowRegister()
     {
         SubPage.Content = new PageAanmelden();
+        HeaderTitel.Text = "Aanmelden";
     }
 
     public void ShowLogin()
     {
         SubPage.Content = new PageInloggen();
+        HeaderTitel.Text = "Inloggen";
     }
 }
