2b1913ecd18423d199562d8483433a9d86366b65
Tim de Boer <timdeboer67@gmail.com>
2025-12-04 14:32:03 +0100

    Revert "Feature/header"

--- Typotrainer/Typotrainer/Views/MainPage.xaml.cs
+++ Typotrainer/Typotrainer/Views/MainPage.xaml.cs
@@ -3,72 +3,73 @@ using Typotrainer.Services;
 
 public partial class MainPage : ContentPage
 {
+    readonly DatabaseService _db;   // add a field for your service
 
     public MainPage()
     {
         InitializeComponent();
-        SubPage.Content = new PageDashboard(); // Dashboard als startpagina
-        HeaderTitel.Text = "Dashboard";
+        _db = new DatabaseService(); // or pass in your connection string here
     }
 
+    // existing navigation handlers
     public void PageInloggenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageInloggen();
-        HeaderTitel.Text = "Inloggen";
     }
 
     public void PageAanmeldenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageAanmelden();
-        HeaderTitel.Text = "Aanmelden";
     }
 
     public void PageDashboardClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageDashboard();
-        HeaderTitel.Text = "Dashboard";
     }
 
     public void PageOefeningClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageOefening();
-        HeaderTitel.Text = "Oefening";
     }
 
     public void PageAdaptieveOefeningClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageAdaptieveOefening();
-        HeaderTitel.Text = "Adaptieve oefening";
     }
 
     public void PageResultatenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageResultaten();
-        HeaderTitel.Text = "Resultaten";
     }
 
     public void PageInstellingenClicked(object sender, EventArgs e)
     {
         SubPage.Content = new PageInstellingen();
-        HeaderTitel.Text = "Instellingen";
+    }
+
+    // NEW: run DB test automatically when page appears
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        DBLabel.Text = "Testing...";
+        string result = await _db.DatabaseTest();
+        DBLabel.Text = result;
     }
 
     // Publieke methodes voor navigatie vanuit andere pagina's
     public void ShowDashboard()
     {
         SubPage.Content = new PageDashboard();
-        HeaderTitel.Text = "Dashboard";
     }
 
     public void ShowRegister()
     {
         SubPage.Content = new PageAanmelden();
-        HeaderTitel.Text = "Aanmelden";
     }
 
     public void ShowLogin()
     {
         SubPage.Content = new PageInloggen();
-        HeaderTitel.Text = "Inloggen";
     }
 }
