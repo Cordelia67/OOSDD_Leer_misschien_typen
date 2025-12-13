715b9092f324dce38c331a513f01b86af0aca937
s1218918 <s1218918@student.windesheim.nl>
2025-12-13 16:46:21 +0100

    SentenceserviceTests opgezet, één test al toegevoegd om zeker te zijn dat het werkt

---
+++ Typotrainer/Typotrainer.Tests/SentenceServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Moq;
+using Typotrainer.Core.Services;
+using Typotrainer.Core.Models;
+using Typotrainer.Core.Interfaces;
+using System.Text;
+
+namespace Typotrainer.Tests
+{
+    [TestFixture]
+    public class SentenceServiceTests
+    {
+        private Mock<IFileProvider> _mockFileProvider;
+        private SentenceService _sentenceService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockFileProvider = new Mock<IFileProvider>();
+
+            // Set up the mock to return test data for each difficulty
+            SetupMockFileProvider("makkelijk.txt", "Makkelijke zin 1\nMakkelijke zin 2\nMakkelijke zin 3");
+            SetupMockFileProvider("middelmatig.txt", "Middelmatige zin 1\nMiddelmatige zin 2/nMiddelmatige zin 3");
+            SetupMockFileProvider("moeilijk.txt", "Moeilijke zin 1\nMoeilijke zin 2\nMoeilijke zin 3");
+
+            _sentenceService = new SentenceService(_mockFileProvider.Object);
+        }
+
+        private void SetupMockFileProvider(string fileName, string content)
+        {
+            // Maak de string tot een stream, dit gebeurt in de normale code automatisch
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+
+            _mockFileProvider
+                .Setup(x => x.OpenAppPackageFileAsync(fileName))
+                .ReturnsAsync(stream);
+        }
+
+        [Test]
+        public void GetRandomSentence_Makkelijk_ReturnsEasySentence()
+        {
+            // Act
+            string result = _sentenceService.GetRandomSentence(Difficulty.Easy);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.StartWith("Makkelijke zin"));
+        }
+    }
+}
