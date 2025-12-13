b780703d8fddd3334e0dc9c0866b289e63e6309e
s1218918 <s1218918@student.windesheim.nl>
2025-12-13 16:16:33 +0100

    Verdere tests voor typingservice toegevoegd, checkt reacties up null, spatie, -1 index, index out of range, en een heel woord

--- Typotrainer/Typotrainer.Tests/TypingServiceTests.cs
+++ Typotrainer/Typotrainer.Tests/TypingServiceTests.cs
@@ -16,7 +16,7 @@ namespace Typotrainer.Core.Tests
         }
 
         [Test]
-        public void IsCorrectLetter_WithCorrectLetter_ReturnsTrue()
+        public void IsCorrectLetter_CorrectLetter_ReturnsTrue()
         {
             // Arrange 
             string correctZin = "Hallo";
@@ -31,7 +31,7 @@ namespace Typotrainer.Core.Tests
         }
 
         [Test]
-        public void IsCorrectLetter_WithIncorrectLetter_ReturnsFalse()
+        public void IsCorrectLetter_IncorrectLetter_ReturnsFalse()
         {
             // Arrange
             string correctZin = "Hallo";
@@ -43,6 +43,92 @@ namespace Typotrainer.Core.Tests
 
             // Assert
             Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsCorrectLetter_NullString_ReturnsFalse()
+        {
+            // Arrange
+            string correctZin = null;
+            int index = 0;
+            char typedChar = 'H';
+
+            // Act
+            bool result = _typingService.IsCorrectLetter(correctZin, index, typedChar);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsCorrectLetter_EmptyLetter_ReturnsFalse()
+        {
+            // Arrange
+            string correctZin = "Hallo";
+            int index = 0;
+            char typedChar = ' ';
+
+            // Act
+            bool result = _typingService.IsCorrectLetter(correctZin, index, typedChar);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsCorrectLetter_NegativeIndex_ReturnsFalse()
+        {
+            // Arrange
+            string correctZin = "Hello";
+            int index = -1;
+            char typedChar = 'H';
+
+            // Act
+            bool result = _typingService.IsCorrectLetter(correctZin, index, typedChar);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsCorrectLetter_IndexOutOfRange_ReturnsFalse()
+        {
+            // Arrange
+            string correctZin = "Hello";
+            int index = 10; // beyond the string length
+            char typedChar = 'H';
+
+            // Act
+            bool result = _typingService.IsCorrectLetter(correctZin, index, typedChar);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void IsCorrectLetter_CorrectFullWord_ReturnsTrue()
+        {
+            // Arrange
+            string correctZin = "Test";
+
+            // Act & Assert
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 0, 'T'), Is.True);
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 1, 'e'), Is.True);
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 2, 's'), Is.True);
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 3, 't'), Is.True);
+        }
+
+        [Test]
+        public void IsCorrectLetter_IncorrectFullWord_ReturnsExpectedResults()
+        {
+            // Arrange
+            string correctZin = "Test";
+
+            // Act & Assert
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 0, 'T'), Is.True);
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 1, 'a'), Is.False);
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 2, 's'), Is.True);
+            Assert.That(_typingService.IsCorrectLetter(correctZin, 3, 't'), Is.True);
         }
     }
 }
