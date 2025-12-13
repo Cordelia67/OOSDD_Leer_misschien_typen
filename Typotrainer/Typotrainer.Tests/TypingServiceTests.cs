e310b0ffe3604bcc3d975785ecb6b978f5e9f6ef
s1218918 <s1218918@student.windesheim.nl>
2025-12-13 15:57:58 +0100

    Twee typingservice tests toegevoegd. Errors in Typotrainer.csproj opgelost, de .txt bestanden met zinnen stonden meerdere keren erin en met fouten. Hiervoor veroorzaakte dit alleen een waarschuwing, maar bij het runnen van tests zorgde het dat er geen build gemaakt kon worden.

---
+++ Typotrainer/Typotrainer.Tests/TypingServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Typotrainer.Core.Services;
+
+namespace Typotrainer.Core.Tests
+{
+    [TestFixture]
+    public class TypingServiceTests
+    {
+        private TypingService _typingService;
+
+        // Setup runt voor elke test
+        [SetUp]
+        public void SetUp()
+        {
+            _typingService = new TypingService();
+        }
+
+        [Test]
+        public void IsCorrectLetter_WithCorrectLetter_ReturnsTrue()
+        {
+            // Arrange 
+            string correctZin = "Hallo";
+            int index = 0;
+            char typedChar = 'H';
+
+            // Act 
+            bool result = _typingService.IsCorrectLetter(correctZin, index, typedChar);
+
+            // Assert 
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void IsCorrectLetter_WithIncorrectLetter_ReturnsFalse()
+        {
+            // Arrange
+            string correctZin = "Hallo";
+            int index = 0;
+            char typedChar = 'h'; // kleine letter in plaats van hoofdletter
+
+            // Act
+            bool result = _typingService.IsCorrectLetter(correctZin, index, typedChar);
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
